89c4b9390c814d881f584393230ad58f174102d0
Injung Hwang <sinban04@gmail.com>
2018-01-26 14:23:21 +0900

    Update the installing part depending on the system

--- vim.sh
+++ vim.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install curl exuberant-ctags cscope
+
+if [ "${os_type}" = "Linux" ];
+then
+  echo Install vim and dependencies with apt ...
+  sudo apt-get install curl exuberant-ctags cscope
+elif [ "${os_type}" = "Darwin" ];
+then
+  echo Install vim and dependencies with Homebrew ...
+  brew install curl ctags cscope
+fi
+
 cp vimrc ~/.vimrc
 
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/bundle && \
@@ -11,15 +21,17 @@ git clone https://github.com/majutsushi/tagbar
 
 os_type=$(uname)
 
-#echo ${os_type}
-echo
+echo This system is ${os_type}
 
-if [ ${os_type} = "Linux" ];
+
+if [ "${os_type}" = "Linux" ];
 then
   echo If you want to change ls color, add to bashrc the following
   echo LS_COLORS="di=00;36:fi=00;37"
-elif [ ${os_type} = "Darwin" ];
+elif [ "${os_type}" = "Darwin" ];
 then
-  echo Macos 
+  cp ./bash_profile ~/.bash_profile
+  cp ./bashrc_mac ~/.bashrc
+
 fi
 
